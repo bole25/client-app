003573dd22ac757641426c35f64dc3d6896677d3
bole25 <mbojan13@gmail.com>
2019-11-18 15:04:54 +0100

    dodat je login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'app-login',
+  templateUrl: '/login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+  constructor(public router: Router) {}
+
+}
