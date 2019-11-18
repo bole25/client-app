c9c936c5c56dc6223c046e7c84242d02dd0f0a48
bole25 <mbojan13@gmail.com>
2019-11-18 15:05:42 +0100

    uradjeno routiranje za login stranici (nema prikaza home page)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Router, RouterModule, Routes} from '@angular/router';
+import { LoginComponent} from './login/login.component';
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent }
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(router: Router) {}
+}
