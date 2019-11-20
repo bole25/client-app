515287a5f0d0472114e4e1559668eb6796003dba
Milica Medic <milica.medic@gmail.com>
2019-11-20 20:27:27 +0100

    Established communication between Angular and Spring servers

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export class User {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  address: string;
+  city: string;
+  country: string;
+  ssn: string;
+  phoneNumber: string;
+}
