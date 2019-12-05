0cbe5e886beb43b9125981049cca43218e709993
anahnas <tomic.ana36@gmail.com>
2019-12-05 19:31:27 +0100

    mapping clinic's database to table in page for registering new clinic.

--- src/app/users/ccadminregisterclinic.component.ts
+++ src/app/users/ccadminregisterclinic.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import {Clinic} from '../models/clinic.model';
 import {CcadminregisterclinicService} from './ccadminregisterclinic.service';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-users',
@@ -9,13 +10,35 @@ import {CcadminregisterclinicService} from './ccadminregisterclinic.service';
 
 })
 
-export class CcadminRegisterClinicComponent {
+export class CcadminRegisterClinicComponent implements OnInit {
+
   clinicName: string;
   address: string;
   clinic: Clinic;
+  listofClinics: Clinic[] = [];
+  clinics: Set<Clinic>;
+  angForm: FormGroup;
 
 
   constructor(private router: Router, private route: ActivatedRoute, private service: CcadminregisterclinicService) {
     this.clinic = new Clinic();
+    this.clinics = new Set<Clinic>();
+  }
+
+  ngOnInit() {
+
+    /*this.service.getAllClinics().subscribe(data => {
+      this.listofClinics = data;
+    });*/
+    this.service.getRequests().subscribe(data => {
+      this.clinics = data;
+    });
+
+  }
+
+
+  onSubmit(): void {
+
+    this.service.save(this.clinic).subscribe(result => this.router.navigate(['/clinics']));
   }
 }
